1f502f5649ddddc30b258c0f1c909c098d41e386
GitSakro <arkadiusz.jastrzebski@ericsson.com>
2018-06-13 15:30:25 +0200

    Adding button basic Handling

--- app/src/main/java/si/smarttranslator/MainActivity.java
+++ app/src/main/java/si/smarttranslator/MainActivity.java
@@ -2,12 +2,28 @@ package si.smarttranslator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton micButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        micButton = this.findViewById(R.id.microphoneSwitcher);
+
+        micButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    micButton.setImageResource(R.drawable.mic_recording);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    micButton.setImageResource(R.drawable.mic_off);
+                }
+                return true;
+            }
+        });
     }
 }
